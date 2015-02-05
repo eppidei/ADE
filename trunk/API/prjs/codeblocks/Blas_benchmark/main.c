90553048d9efaddde75f8c53c1209f376df9db9b
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-02-05 17:06:09 +0000

    Corrected problem on complex blas 3 (Set Alpha and Set Beta), + minor improvements

--- trunk/API/prjs/codeblocks/Blas_benchmark/main.c
+++ trunk/API/prjs/codeblocks/Blas_benchmark/main.c
@@ -12,14 +12,15 @@ int main()
 
     ADE_BENCH_T test_cases[1]={ADE_BLAS_L3_BM};
     ADE_BENCH_MAT_T mat_type[2]={ADE_BENCH_REAL,ADE_BENCH_CPLX};
+    FILE* p_debug_out=stdout;
     ADE_INT32_T ret=0;
 
-    ADE_INT32_T n_rows_A[10]={2,4};//{2,10,50,100,500,1000,2000,3000,5000,10000};
-    ADE_INT32_T n_cols_A[10]={2,4};//{2,10,50,100,500,1000,2000,3000,5000,10000};
-    ADE_INT32_T n_cols_B[10]={2,4};//{2,10,50,100,500,1000,2000,3000,5000,10000};
+    ADE_INT32_T n_rows_A[10]={2};//{2,10,50,100,500,1000,2000,3000,5000,10000};
+    ADE_INT32_T n_cols_A[10]={2};//{2,10,50,100,500,1000,2000,3000,5000,10000};
+    ADE_INT32_T n_cols_B[10]={2};//{2,10,50,100,500,1000,2000,3000,5000,10000};
 
 
-ret=blas3_test_procedure(test_cases,1,mat_type,2,n_rows_A,n_cols_A,n_cols_B,2);
+ret=blas3_test_procedure(test_cases,1,mat_type,2,n_rows_A,n_cols_A,n_cols_B,1,p_debug_out);
 
 
 
