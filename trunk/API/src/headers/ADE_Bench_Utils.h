43a802e2e35d508f81bb96fe704f43fc240d922e
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-02-23 15:34:36 +0000

    Added extern C to bench function, substituted main.c with main.cpp in blas_benchmark deleted SCDF_Host local function replication of the ones in shared C function

--- trunk/API/src/headers/ADE_Bench_Utils.h
+++ trunk/API/src/headers/ADE_Bench_Utils.h
@@ -11,8 +11,14 @@ void custom_caABpbC (ADE_blas_level3_T *p_blas_l3,ADE_CPLX_T *p_C);
 int blas3_configuration(ADE_blas_level3_T* p_Blas_l3,ADE_CHAR_T TRANSA,ADE_CHAR_T TRANSB,ADE_INT32_T M,
                          ADE_INT32_T N,ADE_INT32_T K,ADE_INT32_T LDA,ADE_INT32_T LDB,ADE_INT32_T LDC,
                          ADE_FLOATING_T *p_alpha,ADE_FLOATING_T *p_beta,ADE_FLOATING_T *p_A,ADE_FLOATING_T *p_B,ADE_FLOATING_T *p_C);
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
 ADE_INT32_T blas3_test_procedure(ADE_BENCH_T *test_cases,ADE_UINT32_T n_tests,ADE_BENCH_MAT_T *mat_type,ADE_UINT32_T n_types,ADE_INT32_T *n_rows_A,ADE_INT32_T *n_cols_A,ADE_INT32_T *n_cols_B,ADE_INT32_T n_dim_cases,FILE* p_fid);
 
-
+#ifdef __cplusplus
+    }   /* extern "C" */
+#endif
 
 #endif //_ADE_BENCH_UTILS_H
