b49a009ac6d69494e9fa0a6af4fe4d1a330a705f
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-02-26 23:49:49 +0000

    readded main.cpp in blas_benchmark that "disappered"

---
+++ API/prjs/codeblocks/Blas_benchmark/main.cpp
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "headers/ADE_defines.h"
+#include "headers/ADE_typedefs.h"
+#include "headers/ADE_Bench_Utils.h"
+
+
+
+int main()
+{
+
+    ADE_BENCH_T test_cases[1]={ADE_BLAS_L3_BM};
+    ADE_BENCH_MAT_T mat_type[2]={ADE_BENCH_REAL,ADE_BENCH_CPLX};
+    FILE* p_debug_out=stdout;
+    ADE_INT32_T ret=0;
+
+    ADE_INT32_T n_rows_A[10]={2};//{2,10,50,100,500,1000,2000,3000,5000,10000};
+    ADE_INT32_T n_cols_A[10]={2};//{2,10,50,100,500,1000,2000,3000,5000,10000};
+    ADE_INT32_T n_cols_B[10]={2};//{2,10,50,100,500,1000,2000,3000,5000,10000};
+
+
+ret=blas3_test_procedure(test_cases,1,mat_type,2,n_rows_A,n_cols_A,n_cols_B,1,p_debug_out);
+
+
+
+    return ret;
+}
+
+
