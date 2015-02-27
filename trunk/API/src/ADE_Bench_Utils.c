d4564afbe54b5cc9ba4c445663e74ca53b3cf0b6
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-02-27 00:38:37 +0000

    Added FFt_test_procedure, resolved bug in checker that showed always the same time

--- trunk/API/src/ADE_Bench_Utils.c
+++ trunk/API/src/ADE_Bench_Utils.c
@@ -67,7 +67,7 @@ time_sum2=(*(bench_struct->p_stop_2)).tv_nsec-(*(bench_struct->p_start_2)).tv_ns
             fprintf(p_fid,"\n");
             fprintf(p_fid,"TEST %d passed with residue %f and tolerance %f\n",test_id,result,tol);
             fprintf(p_fid,"Blas Implementation took  %lf ns \n",time_sum1);
-            fprintf(p_fid,"Custom Implementation took  %lf ns \n",time_sum1);
+            fprintf(p_fid,"Custom Implementation took  %lf ns \n",time_sum2);
 
     }
     else
@@ -542,7 +542,7 @@ time_sum2=(*(bench_struct->p_stop_2)).tv_nsec-(*(bench_struct->p_start_2)).tv_ns
             fprintf(p_fid,"\n");
             fprintf(p_fid,"TEST %d passed with residue %f and tolerance %f\n",test_id,result,tol);
             fprintf(p_fid,"FFT LIBRARY took  %lf ns \n",time_sum1);
-            fprintf(p_fid,"Custom FFT took  %lf ns \n",time_sum1);
+            fprintf(p_fid,"Custom FFT took  %lf ns \n",time_sum2);
 
     }
     else
@@ -567,3 +567,172 @@ time_sum2=(*(bench_struct->p_stop_2)).tv_nsec-(*(bench_struct->p_start_2)).tv_ns
 
 
 }
+
+ADE_INT32_T fft_test_procedure(ADE_FFT_TYPE_T fft_type,ADE_UINT32_T *p_dim,ADE_INT32_T n_dim_cases,FILE* p_fid)
+{
+
+    ADE_FFT_T *p_fft;
+    ADE_MATLAB_T *p_mat;
+    ADE_UINT32_T buff_len;
+    ADE_SIZE_T size_fft_in,size_fft_out;
+    ADE_VOID_T *p_in;
+    ADE_VOID_T *p_out;
+    ADE_UINT32_T i=0,buff_len_idx=0;
+
+    ADE_INT32_T lin,col;
+    ADE_API_RET_T ret=ADE_DEFAULT_RET;
+    double *p_test;
+    float *p_test2;
+     ADE_FLOATING_T tolerance=1e-4;
+    ADE_VOID_T *p_fft_custom=NULL;
+     struct timespec start_fft, stop_fft,start_cust, stop_cust,res;
+    int ret_time=0;
+    int err_code=0;
+#if ( (ADE_TARGET==ADE_PC_DEBUG_MATLAB) || (ADE_TARGET==ADE_PC_DEBUG_NORMAL) || (ADE_TARGET==ADE_PC_RELEASE) || (ADE_TARGET==ADE_ANDROID) )
+    clockid_t clock_id=CLOCK_MONOTONIC;//CLOCK_REALTIME,CLOCK_PROCESS_CPUTIME_ID,CLOCK_THREAD_CPUTIME_ID
+#endif
+   bench_times_T bench_times_int;
+
+    bench_times_int.p_start_1=&start_fft;
+    bench_times_int.p_stop_1=&stop_fft;
+    bench_times_int.p_start_2=&start_cust;
+    bench_times_int.p_stop_2=&stop_cust;
+    bench_times_int.p_res=&res;
+
+#if ( (ADE_TARGET==ADE_PC_DEBUG_MATLAB) || (ADE_TARGET==ADE_PC_DEBUG_NORMAL) || (ADE_TARGET==ADE_PC_RELEASE) || (ADE_TARGET==ADE_ANDROID) )
+
+    ret_time=clock_getres(clock_id,&res);
+    if (ret_time==-1)
+    {
+        err_code=errno;
+        fprintf(p_fid,"Failed setting clock %d : error %d\n",clock_id,err_code);
+        return -1;
+    }
+#endif
+
+/******** Cycle on dimension*************/
+
+    for (buff_len_idx=0; buff_len_idx<n_dim_cases; buff_len_idx++)
+    {
+
+        buff_len=p_dim[buff_len_idx];
+
+        /*********************** IN-OUT BUFF MEM ALLOC ********************************/
+
+
+        if (fft_type==ADE_FFT_C2C)
+        {
+            size_fft_in=buff_len*sizeof(ADE_FFTCPLX_T);
+            p_in=fftw_malloc(size_fft_in);
+            if (p_in==NULL)
+            {
+                ADE_PRINT_ERRORS(ADE_MEM,p_in,"%p",ADE_FFT_TEST_Procedure);
+                return ADE_E31;
+            }
+            size_fft_out=buff_len*sizeof(ADE_FFTCPLX_T);
+            p_out=fftw_malloc(size_fft_out);
+            if (p_out==NULL)
+            {
+                ADE_PRINT_ERRORS(ADE_MEM,p_out,"%p",ADE_FFT_TEST_Procedure);
+                return ADE_E31;
+            }
+            p_fft_custom=malloc(size_fft_in);
+            if (p_fft_custom==NULL)
+            {
+                ADE_PRINT_ERRORS(ADE_MEM,p_fft_custom,"%p",ADE_FFT_TEST_Procedure);
+                return ADE_E31;
+            }
+        }
+        else if (fft_type==ADE_FFT_R2C)
+        {
+            size_fft_in=buff_len*sizeof(ADE_FLOATING_T);
+            p_in=fftw_malloc(size_fft_in);
+            if (p_in==NULL)
+            {
+                ADE_PRINT_ERRORS(ADE_MEM,p_in,"%p",ADE_Fft_Init);
+                return ADE_E31;
+            }
+            size_fft_out=(buff_len/2+1)*sizeof(ADE_FFTCPLX_T);
+            p_out=fftw_malloc(size_fft_out);
+            if (p_out==NULL)
+            {
+                ADE_PRINT_ERRORS(ADE_MEM,p_out,"%p",ADE_Fft_Init);
+                return ADE_E31;
+            }
+            /*2check*/
+            p_fft_custom=malloc(size_fft_in);
+            if (p_fft_custom==NULL)
+            {
+                ADE_PRINT_ERRORS(ADE_MEM,p_fft_custom,"%p",ADE_FFT_TEST_Procedure);
+                return ADE_E31;
+            }
+            /**/
+
+        }
+
+        /******************* FFT SETUP ****************************/
+
+        ret=ADE_Fft_Init(&p_fft, buff_len);
+        ret=ADE_Fft_Configure(p_fft,fft_type, ADE_FFT_FORWARD,p_in,p_out);
+
+        /******************* INPUT FILLING ********************************/
+
+        if (fft_type==ADE_FFT_C2C)
+        {
+
+
+            for(i=0; i<buff_len; i++)
+            {
+
+                ((ADE_CPLX_T*)p_in)[i]=ADE_cset((rand()%10),(rand()%10));
+            }
+
+        }
+        else if (fft_type==ADE_FFT_R2C)
+        {
+           ((ADE_FLOATING_T*)p_in)[i]=(ADE_FLOATING_T)(rand()%10);
+        }
+
+        memcpy(p_fft_custom,p_in,size_fft_in);
+
+
+
+
+        /******************** FFT CALCULATION ******************************/
+#if ( (ADE_TARGET==ADE_PC_DEBUG_MATLAB) || (ADE_TARGET==ADE_PC_DEBUG_NORMAL) || (ADE_TARGET==ADE_PC_RELEASE) || (ADE_TARGET==ADE_ANDROID) )
+                         clock_gettime(clock_id,&start_fft);
+//#elif (ADE_TARGET==ADE_IOS)
+//                            TICK;
+#endif
+         ret=ADE_Fft_Step(p_fft);
+         #if ( (ADE_TARGET==ADE_PC_DEBUG_MATLAB) || (ADE_TARGET==ADE_PC_DEBUG_NORMAL) || (ADE_TARGET==ADE_PC_RELEASE) || (ADE_TARGET==ADE_ANDROID) )
+                          clock_gettime(clock_id,&stop_fft);
+
+                          clock_gettime(clock_id,&start_cust);
+//#elif (ADE_TARGET==ADE_IOS)
+//                            TOCK;
+//                            TICK;
+#endif
+         custom_FFT((ADE_FLOATING_T*)p_fft_custom-1,buff_len, ADE_CUSTOM_FFT_FORWARD);
+#if ( (ADE_TARGET==ADE_PC_DEBUG_MATLAB) || (ADE_TARGET==ADE_PC_DEBUG_NORMAL) || (ADE_TARGET==ADE_PC_RELEASE) || (ADE_TARGET==ADE_ANDROID) )
+                        clock_gettime(clock_id,&stop_cust);
+//#elif (ADE_TARGET==ADE_IOS)
+//                            TOCK;
+#endif
+
+       fft_checker(p_fft,p_fft_custom,tolerance,buff_len_idx,fft_type,&bench_times_int,p_fid);
+
+        /************ FFT RELEASE***************/
+        fftw_free(p_in);
+        fftw_free(p_out);
+        free(p_fft_custom);
+        ADE_Fft_Release(p_fft);
+
+    }
+    return ADE_DEFAULT_RET;
+}
+
+
+
+
+
