0681ff99857ee0f351fb71b036ffb845b15611a4
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-02-28 19:35:17 +0000

    Improved Ade_fft.c , inserted also FFT_step for IOS

--- trunk/API/src/ADE_fft.c
+++ trunk/API/src/ADE_fft.c
@@ -7,11 +7,13 @@
 
 
 
+/************** Private Methods Headers ******************/
 
-
-static ADE_VOID_T ADE_Fft_SetInBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_inbuff);
-static ADE_VOID_T ADE_Fft_SetOutBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_outbuff);
-static ADE_API_RET_T ADE_Fft_SetPlan(ADE_FFT_T* p_fft,ADE_FFT_TYPE_T fft_type, ADE_FFT_DIRECTION_T fft_dir);
+static ADE_API_RET_T ADE_Fft_SetInBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_inbuff);
+static ADE_API_RET_T ADE_Fft_SetOutBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_outbuff);
+static ADE_API_RET_T ADE_Fft_SetPlan(ADE_FFT_T* p_fft);
+static ADE_API_RET_T ADE_Fft_SetDirection(ADE_FFT_T* p_fft,ADE_FFT_DIRECTION_T fft_dir);
+static ADE_API_RET_T ADE_Fft_SetType(ADE_FFT_T* p_fft,ADE_FFT_TYPE_T fft_type);
 
 
 ADE_API_RET_T ADE_Fft_Init(ADE_FFT_T** dp_this,ADE_UINT32_T buff_len)
@@ -33,14 +35,15 @@ ADE_API_RET_T ADE_Fft_Init(ADE_FFT_T** dp_this,ADE_UINT32_T buff_len)
 
                 if (ret_fftw<0)
                 {
-                    ADE_PRINT_ERRORS(ADE_INCHECKS,ret_fftw,"%d",ADE_Fft_Init);
+                    ADE_PRINT_ERRORS(ADE_RETCHECKS,ret_fftw,"%d",ADE_Fft_Init);
                     return ADE_E33;
                 }
 
             #endif
         #endif
 
-
+         p_this->direction=ADE_FFT_INVALID_DIR;
+         p_this->type=ADE_FFT_INVALID_TYPE;
 
         *dp_this=p_this;
     }
@@ -69,9 +72,11 @@ ADE_API_RET_T ADE_Fft_Configure(ADE_FFT_T* p_fft,ADE_FFT_TYPE_T fft_type, ADE_FF
                 fftw_plan_with_nthreads(ADE_FFTW_NTHREADS);
           #endif
       #endif
+    ADE_Fft_SetType(p_fft,fft_type);
+    ADE_Fft_SetDirection(p_fft,fft_dir);
 
 
-     plan_ret=ADE_Fft_SetPlan(p_fft,fft_type,fft_dir);
+     plan_ret=ADE_Fft_SetPlan(p_fft);
 
      if (plan_ret<0)
      {
@@ -84,163 +89,11 @@ ADE_API_RET_T ADE_Fft_Configure(ADE_FFT_T* p_fft,ADE_FFT_TYPE_T fft_type, ADE_FF
 
 }
 
-static ADE_VOID_T ADE_Fft_SetInBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_inbuff)
-{
-    p_fft->p_in=p_inbuff;
-
-}
-
-static ADE_VOID_T ADE_Fft_SetOutBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_outbuff)
-{
-    p_fft->p_out=p_outbuff;
-
-}
-
-
-static ADE_API_RET_T ADE_Fft_SetPlan(ADE_FFT_T* p_fft,ADE_FFT_TYPE_T fft_type, ADE_FFT_DIRECTION_T fft_dir)
-{
-
-ADE_kFFTRadix_T Ios_radix=kFFTRadix2;
-
-/*** In-Place or not in-place is controlled just with pointers i.e for in-place set p_in=p_out externally**/
-
-      #if (ADE_FFT_IMP==ADE_USE_FFTW)
-        if (fft_type==ADE_FFT_C2C)
-        {
-            if (fft_dir==ADE_FFT_FORWARD)
-            {
-                #if (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
-                    p_fft->plan = fftw_plan_dft_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,FFTW_FORWARD,ADE_FFTW_PLAN_FLAGS);
-                #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
-                    p_fft->plan = fftwf_plan_dft_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,FFTW_FORWARD,ADE_FFTW_PLAN_FLAGS);
-                #else
-
-                    ADE_DEFINE_ERROR(ADE_FP_PRECISION)
-
-                #endif
-
-            }
-            else if (fft_dir==ADE_FFT_BACKWARD)
-            {
-                 #if (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
-                     p_fft->plan = fftw_plan_dft_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,FFTW_BACKWARD,ADE_FFTW_PLAN_FLAGS);
-                 #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
-                      p_fft->plan = fftwf_plan_dft_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,FFTW_BACKWARD,ADE_FFTW_PLAN_FLAGS);
-                 #else
-
-                    ADE_DEFINE_ERROR(ADE_FP_PRECISION)
-
-                #endif
-            }
-            else
-            {
-                ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetPlan);
-                return ADE_E32;
-            }
-
-        }
-        else if (fft_type==ADE_FFT_R2C)
-        {
-            if (fft_dir==ADE_FFT_FORWARD)
-            {
-                 #if (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
-                    p_fft->plan = fftw_plan_dft_r2c_1d(p_fft->buff_len,(ADE_FLOATING_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
-                 #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
-                     p_fft->plan = fftwf_plan_dft_r2c_1d(p_fft->buff_len,(ADE_FLOATING_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
-                  #else
-
-                    ADE_DEFINE_ERROR(ADE_FP_PRECISION)
-
-                #endif
-            }
-            else if (fft_dir==ADE_FFT_BACKWARD)
-            {
-                 ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetPlan);
-                return ADE_E32;
-            }
-            else
-            {
-                ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetPlan);
-                return ADE_E32;
-            }
-
-        }
-        else if (fft_type==ADE_FFT_C2R)
-        {
-            if (fft_dir==ADE_FFT_FORWARD)
-            {
-
-                   ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetPlan);
-                return ADE_E32;
-
-            }
-            else if (fft_dir==ADE_FFT_BACKWARD)
-            {
-                #if (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
-                    p_fft->plan = fftw_plan_dft_c2r_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FLOATING_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
-                #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
-                    p_fft->plan = fftwf_plan_dft_c2r_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FLOATING_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
-                #else
-                    ADE_DEFINE_ERROR(ADE_FP_PRECISION)
-
-                #endif
-            }
-            else
-            {
-                ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetPlan);
-                return ADE_E32;
-            }
-
-        }
-        else
-        {
-            ADE_PRINT_ERRORS(ADE_INCHECKS,fft_type,"%d",ADE_Fft_SetPlan);
-            return ADE_E32;
-        }
-
-        if (p_fft->plan==NULL)
-        {
-            ADE_PRINT_ERRORS(ADE_MEM,p_fft->plan,"%p",ADE_Fft_SetPlan);
-            return ADE_E31;
-        }
-
-        #elif (ADE_FFT_IMP==ADE_USE_ACCEL_FMW_FFT)
-
-            #if (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
-
-                p_fft->p_setup=vDSP_create_fftsetup ( (ADE_vDSP_Length) ceil(log2(p_fft->buff_len)), Ios_radix );
-
-            #elif (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
-
-                p_fft->p_setup=vDSP_create_fftsetupD ( (ADE_vDSP_Length) ceil(log2(p_fft->buff_len)), Ios_radix );
-
-            #else
-              ADE_DEFINE_ERROR(ADE_FP_PRECISION);
-
-            #endif
-
-        if (p_fft->p_setup==NULL)
-        {
-            ADE_PRINT_ERRORS(ADE_MEM,p_fft->p_setup,"%p",ADE_Fft_SetPlan);
-            return ADE_E31;
-        }
-
-        #else
-
-        ADE_DEFINE_ERROR(ADE_FFT_IMP);
-
-        #endif
-
-
-
-        return ADE_DEFAULT_RET;
-}
-
 ADE_API_RET_T ADE_Fft_Step(ADE_FFT_T* p_fft)
 {
     #if (ADE_FFT_IMP==ADE_USE_FFTW)
 
-        #if (ADE_CHECK_INPUTS==1)
+        #if (ADE_CHECK_INPUTS==ADE_CHECK_RETURNS_TRUE)
             if (p_fft->p_in==NULL)
             {
                  ADE_PRINT_ERRORS(ADE_INCHECKS,p_fft->p_in,"%p",ADE_Fft_Step);
@@ -264,6 +117,31 @@ ADE_API_RET_T ADE_Fft_Step(ADE_FFT_T* p_fft)
         #endif
 
         fftw_execute(p_fft->plan);
+
+    #elif (ADE_FFT_IMP==ADE_USE_ACCEL_FMW_FFT)
+
+        if (p_fft->type==ADE_FFT_C2C)
+        {
+            #if (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+                 vDSP_fft_zopD ( p_fft->p_setup,p, p_fft->p_in, 1, p_fft->p_out, 1, log2(p_fft->buff_len), p_fft->direction );
+            #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+                vDSP_fft_zop ( p_fft->p_setup,p, p_fft->p_in, 1, p_fft->p_out, 1, log2(p_fft->buff_len), p_fft->direction );
+                #else
+                    ADE_DEFINE_ERROR(ADE_FP_PRECISION)
+                #endif
+        }
+        else if (p_fft->type==ADE_FFT_R2C)
+        {
+        #if (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+            vDSP_fft_zropD ( p_fft->p_setup,p, p_fft->p_in, 1, p_fft->p_out, 1, log2(p_fft->buff_len), p_fft->direction );
+        #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+            vDSP_fft_zrop ( p_fft->p_setup,p, p_fft->p_in, 1, p_fft->p_out, 1, log2(p_fft->buff_len), p_fft->direction );
+        #else
+            ADE_DEFINE_ERROR(ADE_FP_PRECISION)
+        #endif
+        }
+
+
 
 
     #else
@@ -298,4 +176,180 @@ ADE_VOID_T ADE_Fft_Release(ADE_FFT_T* p_fft)
          ADE_DEFINE_ERROR(ADE_FFT_IMP);
     #endif
     free(p_fft);
+}
+
+
+/******************** Private Methods *****************************/
+
+static ADE_API_RET_T ADE_Fft_SetInBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_inbuff)
+{
+    if (p_inbuff==NULL)
+    {
+        ADE_PRINT_ERRORS(ADE_INCHECKS,p_inbuff,"%p",ADE_Fft_SetInBuff);
+        return ADE_E32;
+
+    }
+    p_fft->p_in=p_inbuff;
+
+    return ADE_DEFAULT_RET;
+
+}
+
+static ADE_API_RET_T ADE_Fft_SetOutBuff(ADE_FFT_T* p_fft,ADE_VOID_T *p_outbuff)
+{
+    if (p_outbuff==NULL)
+    {
+        ADE_PRINT_ERRORS(ADE_INCHECKS,p_outbuff,"%p",ADE_Fft_SetOutBuff);
+        return ADE_E32;
+
+    }
+    p_fft->p_out=p_outbuff;
+ return ADE_DEFAULT_RET;
+}
+
+static ADE_API_RET_T ADE_Fft_SetDirection(ADE_FFT_T* p_fft,ADE_FFT_DIRECTION_T fft_dir)
+{
+    if (fft_dir!=ADE_FFT_FORWARD && fft_dir!=ADE_FFT_BACKWARD)
+    {
+        ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetDirection);
+        return ADE_E32;
+    }
+    else if (p_fft->type==ADE_FFT_R2C && fft_dir==ADE_FFT_BACKWARD)
+    {
+        ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetDirection);
+        return ADE_E32;
+    }
+    else if (p_fft->type==ADE_FFT_C2R && fft_dir==ADE_FFT_FORWARD)
+    {
+        ADE_PRINT_ERRORS(ADE_INCHECKS,fft_dir,"%d",ADE_Fft_SetDirection);
+        return ADE_E32;
+    }
+
+    #if (ADE_FFT_IMP==ADE_USE_FFTW)
+    if (fft_dir==ADE_FFT_FORWARD)
+    {
+        p_fft->direction=ADE_FFTW_FORWARD;
+    }
+    else if (fft_dir==ADE_FFT_BACKWARD)
+        {
+            p_fft->direction=ADE_FFTW_BACKWARD;
+        }
+    #elif (ADE_FFT_IMP==ADE_USE_ACCEL_FMW_FFT)
+        if (fft_dir==ADE_FFT_FORWARD)
+        {
+             p_fft->direction=ADE_vDSP_DFT_FORWARD;
+        }
+        else if (fft_dir==ADE_FFT_BACKWARD)
+        {
+            p_fft->direction=ADE_vDSP_DFT_INVERSE;
+        }
+
+
+    #else
+
+    #endif
+return ADE_DEFAULT_RET;
+}
+
+static ADE_API_RET_T ADE_Fft_SetType(ADE_FFT_T* p_fft,ADE_FFT_TYPE_T fft_type)
+{
+    if (fft_type!=ADE_FFT_C2C && fft_type!=ADE_FFT_C2R && fft_type!=ADE_FFT_R2C && fft_type!=ADE_FFT_R2R)
+    {
+        ADE_PRINT_ERRORS(ADE_INCHECKS,fft_type,"%d",ADE_Fft_SetType);
+        return ADE_E32;
+    }
+    p_fft->type=fft_type;
+return ADE_DEFAULT_RET;
+}
+static ADE_API_RET_T ADE_Fft_SetPlan(ADE_FFT_T* p_fft)
+{
+
+ADE_kFFTRadix_T Ios_radix=ADE_kFFTRadix2;
+
+/*** In-Place or not in-place is controlled just with pointers i.e for in-place set p_in=p_out externally**/
+
+      #if (ADE_FFT_IMP==ADE_USE_FFTW)
+        if (p_fft->type==ADE_FFT_C2C)
+        {
+
+                #if (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
+                    p_fft->plan = fftw_plan_dft_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,p_fft->direction,ADE_FFTW_PLAN_FLAGS);
+                #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+                    p_fft->plan = fftwf_plan_dft_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,p_fft->direction,ADE_FFTW_PLAN_FLAGS);
+                #else
+
+                    ADE_DEFINE_ERROR(ADE_FP_PRECISION)
+
+                #endif
+
+        }
+        else if (p_fft->type==ADE_FFT_R2C)
+        {
+
+             #if (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
+                p_fft->plan = fftw_plan_dft_r2c_1d(p_fft->buff_len,(ADE_FLOATING_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
+             #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+                 p_fft->plan = fftwf_plan_dft_r2c_1d(p_fft->buff_len,(ADE_FLOATING_T*)p_fft->p_in,(ADE_FFTCPLX_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
+              #else
+
+                ADE_DEFINE_ERROR(ADE_FP_PRECISION)
+
+            #endif
+
+
+        }
+        else if (p_fft->type==ADE_FFT_C2R)
+        {
+
+                #if (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
+                    p_fft->plan = fftw_plan_dft_c2r_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FLOATING_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
+                #elif (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+                    p_fft->plan = fftwf_plan_dft_c2r_1d(p_fft->buff_len,(ADE_FFTCPLX_T*)p_fft->p_in,(ADE_FLOATING_T*)p_fft->p_out,ADE_FFTW_PLAN_FLAGS);
+                #else
+                    ADE_DEFINE_ERROR(ADE_FP_PRECISION)
+
+                #endif
+        }
+        else
+        {
+            ADE_PRINT_ERRORS(ADE_INCHECKS,p_fft->type,"%d",ADE_Fft_SetPlan);
+            return ADE_E32;
+        }
+
+        if (p_fft->plan==NULL)
+        {
+            ADE_PRINT_ERRORS(ADE_MEM,p_fft->plan,"%p",ADE_Fft_SetPlan)
+            return ADE_E31;
+        }
+
+        #elif (ADE_FFT_IMP==ADE_USE_ACCEL_FMW_FFT)
+
+            #if (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+
+                p_fft->p_setup=vDSP_create_fftsetup ( (ADE_vDSP_Length) ceil(log2(p_fft->buff_len)), Ios_radix );
+
+            #elif (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
+
+                p_fft->p_setup=vDSP_create_fftsetupD ( (ADE_vDSP_Length) ceil(log2(p_fft->buff_len)), Ios_radix );
+
+            #else
+              ADE_DEFINE_ERROR(ADE_FP_PRECISION)
+
+            #endif
+
+        if (p_fft->p_setup==NULL)
+        {
+            ADE_PRINT_ERRORS(ADE_MEM,p_fft->p_setup,"%p",ADE_Fft_SetPlan)
+            return ADE_E31;
+        }
+
+        #else
+
+        ADE_DEFINE_ERROR(ADE_FFT_IMP)
+
+        #endif
+
+
+
+        return ADE_DEFAULT_RET;
 }
--- trunk/API/src/headers/ADE_fft.h
+++ trunk/API/src/headers/ADE_fft.h
@@ -12,19 +12,27 @@ ADE_DEFINE_ERROR(ADE_FFT_IMP);
 
 struct ADE_FFT_S {
         ADE_UINT32_T buff_len;
-        ADE_VOID_T *p_in;//allocati dentro con ffttw_malloc
-        ADE_VOID_T *p_out;//allocati dentro con ffttw_malloc
+         ADE_FFT_TYPE_T type;
         #if (ADE_FFT_IMP==ADE_USE_FFTW)
+        ADE_VOID_T *p_in;//allocati fuori con ffttw_malloc
+        ADE_VOID_T *p_out;//allocati fuori con ffttw_malloc
+        ADE_FFTW_DIRECTION_T direction;
         fftw_plan plan;
         #elif (ADE_FFT_IMP==ADE_USE_ACCEL_FMW_FFT)
             #if (ADE_FP_PRECISION==ADE_USE_SINGLE_PREC)
+            ADE_DSPSplitComplex *p_in;
+            ADE_DSPSplitComplex *p_out;
             ADE_FFTSetup p_setup;
+            ADE_vDSP_DFT_Direction_T direction;
             #elif (ADE_FP_PRECISION==ADE_USE_DOUBLE_PREC)
+            ADE_DSPDoubleSplitComplex *p_in;
+            ADE_DSPDoubleSplitComplex *p_out;
             ADE_FFTSetupD p_setup;
+            ADE_vDSP_DFT_Direction_T direction;
             #else
             #endif
         #else
-        ADE_DEFINE_ERROR(ADE_FFT_IMP);
+        ADE_DEFINE_ERROR(ADE_FFT_IMP)
         #endif
         };
 
