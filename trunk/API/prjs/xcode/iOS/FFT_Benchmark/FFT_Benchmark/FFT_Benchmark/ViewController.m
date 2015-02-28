5e051a32c3478232f0326bced7c976776173bb9a
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-02-28 12:45:08 +0000

    Added FFT_Benchmark code block project

---
+++ trunk/API/prjs/xcode/iOS/FFT_Benchmark/FFT_Benchmark/FFT_Benchmark/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FFT_Benchmark
+//
+//  Created by nuno on 28/02/15.
+//  Copyright (c) 2015 nuno. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ trunk/API/prjs/xcode/iOS/FFT_Benchmark/FFT_Benchmark/FFT_BenchmarkTests/FFT_BenchmarkTests.m
@@ -0,0 +1,40 @@
+//
+//  FFT_BenchmarkTests.m
+//  FFT_BenchmarkTests
+//
+//  Created by nuno on 28/02/15.
+//  Copyright (c) 2015 nuno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FFT_BenchmarkTests : XCTestCase
+
+@end
+
+@implementation FFT_BenchmarkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
