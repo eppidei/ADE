8f0de4cdd0c56cb217263e2f36512aa8118c4316
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-03-21 02:35:52 +0000

    Revised FIR and IIR Classed NOT TESTED

--- API/src/headers/ADE_iir.h
+++ API/src/headers/ADE_iir.h
@@ -15,19 +15,24 @@ struct ADE_IIR_S
     ADE_FLOATING_T **dp_states; //allocati dentro
     ADE_FLOATING_T *p_gains; //allocati fuori
     ADE_FLOATING_T **dp_section_buffers; //allocati dentro
-    ADE_blas_level1_T *p_Blas_L1;
+    ADE_FLOATING_T *p_tempbuff; //allocati dentro
+    ADE_blas_level1_T **dp_Blas_L1;
+    ADE_IIR_IMP_CHOICE_T filt_imp_type;
     ADE_FILTER_IMPLEMENTATION_T filter_func;
 };
 
-
-ADE_API_RET_T ADE_Iir_Init(ADE_IIR_T** dp_this, ADE_UINT32_T n_SOS_sections,ADE_UINT32_T buff_len);
+/******** Init Methods ***********/
+ADE_API_RET_T ADE_Iir_Init(ADE_IIR_T** dp_this, ADE_UINT32_T n_SOS_sections,ADE_UINT32_T buff_len,ADE_IIR_IMP_CHOICE_T filt_imp_type);
 ADE_VOID_T ADE_Iir_Release(ADE_IIR_T* p_iir);
+/************ Set Methods **************/
 ADE_API_RET_T ADE_Iir_setGains(ADE_IIR_T* p_iir, ADE_FLOATING_T* p_gains);
 ADE_API_RET_T ADE_Iir_setNums(ADE_IIR_T* p_iir, ADE_FLOATING_T** dp_nums);
 ADE_API_RET_T ADE_Iir_setDenoms(ADE_IIR_T* p_iir, ADE_FLOATING_T** dp_denoms);
 ADE_API_RET_T ADE_Iir_setInbuff(ADE_IIR_T* p_iir, ADE_FLOATING_T* p_buff);
 ADE_API_RET_T ADE_Iir_setOutbuff(ADE_IIR_T* p_iir, ADE_FLOATING_T* p_buff);
-ADE_API_RET_T ADE_Iir_setFilt_Implementation(ADE_IIR_T* p_iir,ADE_FILTER_IMP_CHOICE_T filt_imp_type);
+
+
+/******** Processing Methods **********/
 ADE_API_RET_T ADE_Iir_Step(ADE_IIR_T* p_iir);
 
 
