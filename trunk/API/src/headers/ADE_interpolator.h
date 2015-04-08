c466f2178f302475cfd6427e5a6516c7d27dbca6
leonardo <leonardo@33e419c5-de32-43cf-b368-fc94c049cd9f>
2015-04-08 23:11:40 +0000

    Added Interp_decimate tb Corrected a bug (order 0) on fir introduced with the latest refactoring

--- trunk/API/src/headers/ADE_interpolator.h
+++ trunk/API/src/headers/ADE_interpolator.h
@@ -18,7 +18,9 @@ struct ADE_INTERPOLATOR_S
     ADE_FIR_T **dp_fir;
     ADE_IIR_T **dp_iir;
 };
-
+#ifdef __cplusplus
+    extern "C" {
+#endif
 /*************** Init Methods ****************/
 ADE_API_RET_T ADE_Interpolator_Init(ADE_INTERPOLATOR_T **dp_interpolator,ADE_UINT32_T in_buff_len,ADE_UINT32_T upfact,ADE_UINT32_T filter_order,
 ADE_INTERPOLATOR_FILTER_T filt_type, ADE_INTERPOLATOR_ARCH_T arch_type,ADE_UINT32_T n_iir_sections);
@@ -31,4 +33,7 @@ ADE_API_RET_T ADE_Interpolator_Configure(ADE_INTERPOLATOR_T *p_interpolator,ADE_
 ADE_SIZE_T out_buff_size,ADE_FLOATING_T *p_num,ADE_FLOATING_T **dp_num,ADE_FLOATING_T **dp_denoms);
 /****************** Processing Methods ************************/
 ADE_API_RET_T ADE_Interpolator_Step(ADE_INTERPOLATOR_T *p_interpolator);
+#ifdef __cplusplus
+    }   /* extern "C" */
+#endif
 #endif //_ADE_INTERPOLATOR_H
